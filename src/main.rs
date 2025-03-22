f91a9579df394107a5ca232052ee46f6770cc1b4
Sergei Surovtsev <ssurovsev@gmail.com>
2025-03-22 17:44:33 +0300

    upd

--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,172 @@
-#![allow(clippy::too_many_arguments, clippy::type_complexity)]
-#![feature(let_chains)]
-
-mod mujoco_plugin;
-mod mujoco_xml_parser;
-mod physics;
-mod stub_ant_robot;
-
-use avian3d::{
-    prelude::{Collider, RigidBody},
-    PhysicsPlugins,
-};
-use bevy::{
-    color::palettes::css::*,
-    pbr::wireframe::{WireframeConfig, WireframePlugin},
-    prelude::*,
-    render::{
-        settings::{RenderCreation, WgpuFeatures, WgpuSettings},
-        RenderPlugin,
-    },
-};
+use bevy::{color::palettes::css::WHITE, prelude::*};
 use bevy_flycam::*;
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_rapier3d::prelude::*;
+use bevy_rl::*;
+use bevy_stl::StlPlugin;
+use serde::{Deserialize, Serialize};
 
-use crate::mujoco_plugin::*;
-use crate::mujoco_xml_parser::*;
-use crate::stub_ant_robot::spawn_stub_model;
+use bevy_urdf::events::{ControlMotors, LoadRobot, RobotLoaded};
+use bevy_urdf::events::{SensorsRead, SpawnRobot};
+use bevy_urdf::plugin::UrdfPlugin;
+use bevy_urdf::urdf_asset_loader::UrdfAsset;
+
+use rand::Rng;
 
 fn main() {
     App::new()
-        .add_plugins((
-            DefaultPlugins.set(RenderPlugin {
-                render_creation: RenderCreation::Automatic(WgpuSettings {
-                    features: WgpuFeatures::POLYGON_MODE_LINE,
-                    ..default()
-                }),
+        .insert_resource(AIGymState::<Actions, EnvironmentState>::new(
+            AIGymSettings {
+                num_agents: 1,
+                render_to_buffer: false,
+                pause_interval: 0.1,
                 ..default()
-            }),
-            WireframePlugin,
-            PhysicsPlugins::default(),
-            WorldInspectorPlugin::new(),
+            },
         ))
-        .insert_resource(WireframeConfig {
-            global: true,
-            default_color: WHITE.into(),
-        })
-        // .insert_resource(SubstepCount(50))
-        .init_asset::<mujoco_xml_parser::MuJoCoFile>()
-        .init_asset_loader::<mujoco_xml_parser::MuJoCoFileLoader>()
-        .add_systems(
-            Startup,
-            (setup_scene, setup_mujoco_robot.after(setup_scene)),
-        )
-        // MuJoCo path -- currently WIP
-        // .add_systems(
-        //     Update,
-        //     (
-        //         spawn_mujoco_model,
-        //         spawn_mujoco_joints.after(spawn_mujoco_model),
-        //     )
-        //         .run_if(in_state(AppState::Loading)),
-        // )
-        // Stub Ant Robot
-        .add_systems(
-            Update,
-            (spawn_stub_model).run_if(in_state(AppState::Loading)),
-        )
-        // .add_systems(Update, ().run_if(in_state(AppState::Simulation)))
+        .add_plugins((
+            DefaultPlugins,
+            UrdfPlugin,
+            StlPlugin,
+            NoCameraPlayerPlugin,
+            RapierPhysicsPlugin::<NoUserData>::default(),
+            AIGymPlugin::<Actions, EnvironmentState>::default(),
+        ))
         .init_state::<AppState>()
-        .add_plugins(NoCameraPlayerPlugin)
         .insert_resource(MovementSettings {
-            speed: 2.0,
+            speed: 1.0,
             ..default()
         })
+        .insert_resource(UrdfRobotHandle(None))
+        .insert_resource(LastSensortReading(None))
+        .add_systems(Startup, setup)
+        .add_systems(Update, start_simulation.run_if(in_state(AppState::Loading)))
+        .add_systems(
+            Update,
+            (bevy_rl_pause_request, bevy_rl_control_request, read_sensors)
+                .run_if(in_state(AppState::Simulation)),
+        )
         .run();
 }
 
-fn setup_scene(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // light
-    commands.spawn((
-        PointLight {
-            shadows_enabled: true,
-            intensity: 10_000_000.,
-            range: 100.0,
-            shadow_depth_bias: 0.2,
-            ..default()
-        },
-        Transform::from_xyz(8.0, 16.0, 8.0),
-    ));
+// Bevy URDF stuff
 
-    // ground plane
-    let cube_mesh = meshes.add(Cuboid::default());
-    commands.spawn((
-        Mesh3d(cube_mesh.clone()),
-        MeshMaterial3d(materials.add(Color::srgb(0.7, 0.7, 0.8))),
-        Transform::from_xyz(0.0, -2.0, 0.0).with_scale(Vec3::new(100.0, 1.0, 100.0)),
-        RigidBody::Static,
-        Collider::cuboid(1.0, 1.0, 1.0),
-    ));
-    commands.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(1.5, 7.5, 3.5).looking_at(Vec3::new(0., 0., 0.), Vec3::Y),
-        FlyCam,
-    ));
+#[derive(Resource)]
+struct UrdfRobotHandle(Option<Handle<UrdfAsset>>);
+
+#[derive(Resource)]
+struct LastSensortReading(Option<EnvironmentState>);
+
+// Bevy RL stuff
+
+#[derive(Default, Deref, DerefMut, Clone, Deserialize)]
+pub struct Actions(Vec<f64>);
+
+#[derive(Default, Serialize, Clone)]
+pub struct EnvironmentState {
+    pub transforms: Vec<[f32; 7]>,
+    pub joint_angles: Vec<f32>,
 }
 
-fn setup_mujoco_robot(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let mujoco_handle: Handle<MuJoCoFile> = asset_server.load("ant.xml");
-    commands.insert_resource(mujoco_plugin::MuJoCoFileHandle(mujoco_handle));
+// bevy_rl systems
+
+fn bevy_rl_pause_request(
+    mut er_pause: EventReader<EventPause>,
+    mut q_rapier_context_simulation: Query<(&mut RapierConfiguration)>,
+    ai_gym_state: Res<AIGymState<Actions, EnvironmentState>>,
+    last_sensors_readings: Res<LastSensortReading>,
+    mut simulation_state: ResMut<NextState<SimulationState>>,
+) {
+    for _ in er_pause.read() {
+        if let Some(state) = last_sensors_readings.0.clone() {
+            for mut rapier_configuration in q_rapier_context_simulation.iter_mut() {
+                if !rapier_configuration.physics_pipeline_active {
+                    return;
+                }
+                rapier_configuration.physics_pipeline_active = false;
+                simulation_state.set(SimulationState::PausedForControl);
+
+                // Set bevy_rl gym state
+                let mut ai_gym_state = ai_gym_state.lock().unwrap();
+                ai_gym_state.set_env_state(state.clone());
+            }
+        }
+    }
+}
+
+#[allow(unused_must_use)]
+fn bevy_rl_control_request(
+    mut er_control: EventReader<EventControl>,
+    mut q_rapier_context_simulation: Query<(&mut RapierConfiguration)>,
+    mut simulation_state: ResMut<NextState<SimulationState>>,
+    mut ew_control_motors: EventWriter<ControlMotors>,
+    robot_handle: Res<UrdfRobotHandle>,
+) {
+    for control in er_control.read() {
+        println!("control");
+        for mut rapier_configuration in q_rapier_context_simulation.iter_mut() {
+            rapier_configuration.physics_pipeline_active = true;
+            let raw_actions = control.0.clone();
+            println!("simulation now active {}", raw_actions.len());
+
+            if let Some(robot_handle) = robot_handle.0.clone() {
+                for i in 0..raw_actions.len() {
+                    if let Some(unparsed_action) = raw_actions[i].clone() {
+                        let velocities: Vec<f32> = serde_json::from_str(&unparsed_action).unwrap();
+                        println!("velocities: {:?}", velocities);
+                        ew_control_motors.send(ControlMotors {
+                            handle: robot_handle,
+                            velocities: velocities,
+                        });
+                        break;
+                    }
+                }
+            }
+
+            simulation_state.set(SimulationState::Running);
+        }
+    }
+}
+
+fn start_simulation(
+    mut commands: Commands,
+    mut er_robot_loaded: EventReader<RobotLoaded>,
+    mut ew_spawn_robot: EventWriter<SpawnRobot>,
+    mut state: ResMut<NextState<AppState>>,
+    mut simulation_state: ResMut<NextState<SimulationState>>,
+) {
+    for event in er_robot_loaded.read() {
+        ew_spawn_robot.send(SpawnRobot {
+            handle: event.handle.clone(),
+            mesh_dir: event.mesh_dir.clone(),
+        });
+        state.set(AppState::Simulation);
+        simulation_state.set(SimulationState::Running);
+        commands.insert_resource(UrdfRobotHandle(Some(event.handle.clone())));
+    }
+}
+
+fn read_sensors(mut commands: Commands, mut er_read_sensors: EventReader<SensorsRead>) {
+    for event in er_read_sensors.read() {
+        let mut transforms: Vec<[f32; 7]> = Vec::new();
+        for transform in event.transforms.iter() {
+            let t: [f32; 7] = [
+                transform.translation.x,
+                transform.translation.y,
+                transform.translation.z,
+                transform.rotation.x,
+                transform.rotation.y,
+                transform.rotation.z,
+                transform.rotation.w,
+            ];
+
+            transforms.push(t);
+        }
+
+        let environment_state = EnvironmentState {
+            transforms: transforms,
+            joint_angles: event.joint_angles.clone(),
+        };
+        commands.insert_resource(LastSensortReading(Some(environment_state)));
+    }
 }
 
 #[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
@@ -118,4 +174,43 @@ enum AppState {
     #[default]
     Loading,
     Simulation,
+}
+
+#[allow(deprecated)]
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut ew_load_robot: EventWriter<LoadRobot>,
+    mut simulation_state: ResMut<NextState<SimulationState>>,
+) {
+    // Scene
+    commands.insert_resource(AmbientLight {
+        color: WHITE.into(),
+        brightness: 300.0,
+    });
+
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(2.0, 2.0, 2.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        FlyCam,
+    ));
+
+    // ground
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(180., 1.8, 180.))),
+        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
+        Collider::cuboid(90., 0.9, 90.),
+        Transform::from_xyz(0.0, -2.5, 0.0),
+        RigidBody::Fixed,
+    ));
+
+    simulation_state.set(SimulationState::Initializing);
+
+    ew_load_robot.send(LoadRobot {
+        urdf_path: "robots/flamingo_edu/urdf/Edu_v4.urdf".to_string(),
+        mesh_dir: "assets/robots/flamingo_edu/urdf".to_string(),
+    });
 }
