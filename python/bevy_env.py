4d8909523a5a8860e6c23f26797e0ab9c48a6f1f
Sergei Surovtsev <ssurovsev@gmail.com>
2025-04-07 14:25:37 +0300

    WIP

--- python/bevy_env.py
+++ python/bevy_env.py
@@ -2,12 +2,26 @@ import requests
 import numpy as np
 import json
 
-API_STEP = "http://127.0.0.1:7878/step"
-API_STATE = "http://127.0.0.1:7878/state"
-API_RESET = "http://127.0.0.1:7878/reset"
+import gymnasium as gym
+
+API_STEP = "http://127.0.0.1:7879/step"
+API_STATE = "http://127.0.0.1:7879/state"
+API_RESET = "http://127.0.0.1:7879/reset"
+
+OBSERVATION_SIZE = 69
+ACTION_SIZE = 16
 
 
-class BevyRLEnv(object):
+class BevyRLEnv(gym.Env):
+
+    def __init__(self):
+
+        self.observation_space = gym.spaces.Box(
+            low=-np.inf, high=np.inf, shape=(OBSERVATION_SIZE,), dtype=float
+        )
+        self.action_space = gym.spaces.Box(
+            low=-5.0, high=5.0, shape=(ACTION_SIZE,), dtype=float
+        )
 
     def get_obs(self):
         state = requests.get(API_STATE).json()
@@ -17,22 +31,29 @@ class BevyRLEnv(object):
         return transforms
 
     def step_env(self, action):
-        payload = json.dumps([{"action": json.dumps(action)}], indent=4)
+        payload = json.dumps([{"action": json.dumps(list(action))}], indent=4)
         return requests.get(API_STEP, params={"payload": payload}).json()
 
     def step(self, action):
+        print("env.step start", action)
         obs = self.get_obs()
         step_data = self.step_env(action)[0]
+        print("env.step end")
         is_terminated = step_data["is_terminated"]
         reward = step_data["reward"]
 
-        return obs, reward, is_terminated, False, {}, is_terminated
+        
+
+        return obs, reward, is_terminated, False, {}
 
     def reset_env(self):
         requests.get(API_RESET)
 
-    def reset(self):
+    def reset(self, seed):
+        print("env.reset start")
         self.reset_env()
+
+        print("env.reset end ")
 
         return self.get_obs(), {}
 
